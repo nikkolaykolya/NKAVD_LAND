1bbf484cf5efba3448ca6b298a863919862cea11
Andrei <86118324+AnDrE1Leb@users.noreply.github.com>
2021-12-02 10:26:44 +0200

    first commit

---
+++ js/counter.js
@@ -0,0 +1,150 @@
+var count = document.getElementById('counter');
+const btn = document.getElementById('form');
+
+
+
+
+function getRandomValueBetween(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  numb = Math.floor(Math.random() * (max - min + 1)) + min;
+  return numb;
+}
+
+Date.prototype.getWeek = function() {
+  var target = new Date(this.valueOf());
+  var dayNr = (this.getDay() + 6) % 7;
+  target.setDate(target.getDate() - dayNr + 3);
+  var firstThursday = target.valueOf();
+  target.setMonth(0, 1);
+  if (target.getDay() != 4) {
+    target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
+  }
+  return 1 + Math.ceil((firstThursday - target) / 604800000);
+}
+
+var d = new Date();
+
+
+var map = {
+  0: 20,
+  1: 18,
+  2: 17,
+  3: 16,
+  4: 18,
+  5: 17,
+  6: 14,
+  7: 16,
+  8: 15,
+  9: 17,
+  10: 19,
+  11: 18,
+  12: 16,
+  13: 15,
+  14: 14,
+  15: 18,
+  16: 19,
+  17: 17,
+  18: 18,
+  19: 19,
+  20: 20,
+  21: 16,
+  22: 15,
+  23: 14,
+  24: 18,
+  25: 20,
+  26: 19,
+  27: 18,
+  28: 17,
+  29: 18,
+  30: 19,
+  31: 20,
+  32: 17,
+  33: 16,
+  34: 15,
+  35: 18,
+  36: 17,
+  37: 18,
+  38: 16,
+  39: 17,
+  40: 18,
+  41: 19,
+  42: 20,
+  43: 18,
+  44: 19,
+  45: 18,
+  46: 17,
+  47: 19,
+  48: 20,
+  49: 19,
+  50: 16,
+  51: 17,
+  52: 16,
+  53: 15,
+  54: 19,
+};
+
+count.innerHTML = map[d.getWeek()] || 22;
+
+
+
+// var image = document.getElementsByClassName('img_parallax');
+// new simpleParallax(image);
+
+//
+//
+// getCount(47 , 22);
+// getCount(48 , 24);
+// getCount(49 , 21);
+// getCount(50 , 19);
+// getCount(51 , 20);
+// getCount(52 , 23);
+// getCount(53 , 21);
+// getCount(54 , 20);
+// getCount(55 , 20);
+// getCount(1 , 23);
+// getCount(2 , 25);
+// getCount(3 , 24);
+// getCount(4 , 23);
+// getCount(5 , 21);
+// getCount(6 , 19);
+// getCount(7 , 22);
+// getCount(8 , 20);
+// getCount(9 , 23);
+// getCount(10 , 24);
+// getCount(11 , 21);
+// getCount(12 , 20);
+// getCount(13 , 27);
+// getCount(14 , 26);
+// getCount(15 , 24);
+// getCount(16 , 26);
+// getCount(17 , 25);
+// getCount(18 , 22);
+// getCount(19 , 23);
+// getCount(20 , 24);
+// getCount(21 , 20);
+// getCount(22 , 22);
+// getCount(23 , 21);
+// getCount(24 , 22);
+// getCount(25 , 24);
+// getCount(26 , 26);
+// getCount(27 , 25);
+// getCount(28 , 24);
+// getCount(29 , 22);
+// getCount(30 , 24);
+// getCount(31 , 21);
+// getCount(32 , 20);
+// getCount(33 , 22);
+// getCount(34 , 21);
+// getCount(35 , 24);
+// getCount(36 , 26);
+// getCount(37 , 22);
+// getCount(38 , 21);
+// getCount(39 , 20);
+// getCount(40 , 22);
+// getCount(41 , 21);
+// getCount(42 , 24);
+// getCount(43 , 26);
+// getCount(44 , 24);
+// getCount(45 , 25);
+// getCount(46 , 22);
---
+++ js/script.js
@@ -0,0 +1,202 @@
+"use strict"
+
+VANTA.NET({
+	el: "#Visual",
+	mouseControls: true,
+	touchControls: true,
+	gyroControls: false,
+	minHeight: 200.00,
+	minWidth: 200.00,
+	scale: 1.00,
+	scaleMobile: 1.00,
+	color: 0xFFD561,
+	backgroundColor: 0xFF000000,
+	points: 6.00,
+	maxDistance: 22.00,
+	spacing: 20.00
+})
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BleakBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Window: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android() ||
+			isMobile.BlackBerry() ||
+			isMobile.iOS() ||
+			isMobile.Opera() ||
+			isMobile.Window());
+	}
+};
+
+//Меню бургер
+const burgerMenu = document.querySelector('.header__burger');
+var headerMenu;
+var headerLogoWhite;
+var headerLogoDark;
+var body;
+if (burgerMenu) {
+	headerMenu = document.querySelector('.header__menu');
+	headerLogoWhite = document.querySelector('.header__img_color_white');
+	headerLogoDark = document.querySelector('.header__img_color_dark');
+	body = document.querySelector('.body');
+	burgerMenu.addEventListener("click", function (e) {
+		burgerMenu.classList.toggle('_active');
+		headerMenu.classList.toggle('_active');
+		headerLogoWhite.classList.toggle('_active');
+		headerLogoDark.classList.toggle('_active');
+		body.classList.toggle('_lock');
+	});
+}
+
+
+
+//Переворот
+var header = document.getElementById("advantageContainer");
+var btns = document.getElementsByClassName("advantage__box");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    for (var j = 0; j < btns.length; j++) {
+			if (this != btns[j]) {
+				btns[j].dataset.active = "inactive";
+			}
+    }
+    if (this.dataset.active == "active") {
+      this.dataset.active = "inactive";
+    }
+		 else {
+      this.dataset.active = "active";
+    }
+  })
+};
+
+var empty = document.getElementById("empty");
+
+empty.addEventListener(`click`, function () {
+	for (var j = 0; j < btns.length; j++) {
+			btns[j].dataset.active = "inactive";
+
+	}
+})
+
+
+// Инициализируем Swiper
+new Swiper('.block1__content-slider_mobile', {
+	slidesPerView: 2.2,
+	centeredSlides: true,
+	spaceBetween: 30,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	loop: true,
+	slidesPerGroup: 1,
+	freeMode: false,
+});
+
+// Переход
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+	smoothLink.addEventListener('click', function (e) {
+		e.preventDefault();
+		const od = smoothLink.getAttribute('href').replace('#', '');
+		const pxTop = '80';
+		const blockOffset = document.getElementById(od).getBoundingClientRect().top + window.pageYOffset - pxTop;
+		window.scrollTo({ top: blockOffset, behavior: 'smooth' });
+		console.log(window.pageYOffset);
+		if (burgerMenu.classList.contains('_active')) {
+			burgerMenu.classList.toggle('_active');
+			headerMenu.classList.toggle('_active');
+			headerLogoWhite.classList.toggle('_active');
+			headerLogoDark.classList.toggle('_active');
+			body.classList.toggle('_lock');
+		};
+	});
+};
+
+var now = new Date();
+var nowHour = now.getUTCHours();
+var nowDay = now.getDay();
+const formResult = document.getElementById('form_result');
+const formResultText = document.getElementById('form_result_text');
+const circleLoader = document.getElementById('circleLoader')
+const checkMark = document.getElementById('checkmark')
+
+document.addEventListener('DOMContentLoaded', function () {
+	const form = document.getElementById('form');
+	form.addEventListener('submit', formSend);
+
+	async function formSend(e) {
+		e.preventDefault();
+
+		let error = formValidate(form);
+	}
+
+	function formValidate(form) {
+		let error = 0;
+		let formReq = document.querySelectorAll('input');
+		let formMap = {};
+		for (let index = 0; index < formReq.length; index++) {
+			const input = formReq[index];
+			formMap[input.name] = input.value;
+
+			if (!input.classList.contains('_req'))
+				continue;
+
+			formRemoveError(input);
+
+			if (input.classList.contains('_email')) {
+				if (emailIsInvalid(input)) {
+					formAddError(input);
+					error++;
+					console.log("Error email validation");
+
+				}
+			}
+			else {
+				if (input.value === '') {
+					formAddError(input);
+					error++;
+					console.log("Error " + input.name + " validation");
+
+				}
+			}
+		}
+
+		if (error == 0) {
+			console.log(formMap);
+			formResult.style.display = "block";
+			if (nowHour > 7 | nowHour > 16 | nowDay === 6 | nowDay === 0) {
+				formResultText.innerHTML = "Наше рабочее время Пн - Пт с 9:00 до 18:00";
+			} else {
+				formResultText.innerHTML = "Наш менеджер свяжется с вами в течении 15-ти минут!";
+			}
+		}
+
+	}
+	function formAddError(input) {
+		input.parentElement.classList.add('_error');
+		input.classList.add('_error');
+	}
+	function formRemoveError(input) {
+		input.parentElement.classList.remove('_error');
+		input.classList.remove('_error');
+	}
+	function emailIsInvalid(input) {
+		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+	}
+
+
+});
