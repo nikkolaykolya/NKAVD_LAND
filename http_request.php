033d43e6e21ce4c068b724b9e368866b8fba14ac
Nikita Software Engineer <81171496+nikkolaykolya@users.noreply.github.com>
2021-12-06 15:40:36 +0200

    Api key update

--- http_request.php
+++ http_request.php
@@ -30,7 +30,7 @@ class HTTPRequester {
     * @return      HTTP-Response body or an empty string if the request fails or is empty
     */
     public static function HTTPPost( $url, array $data ) {
-        $access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImZhZGM3NWU3OTNlY2VhNjg5ZjYyNzYxNWU0MmM5NjBiZTlhMmUzYWU0ZDUzNTI1NDI5ODk0YzAwOWUwZmFhMTZhODRlYWRjN2Q2NWNjNmJjIn0.eyJhdWQiOiJmZWE0M2RlNi1mYzIwLTQwZjUtOTg5MC1hMzI0NjM1ZWRlNDAiLCJqdGkiOiJmYWRjNzVlNzkzZWNlYTY4OWY2Mjc2MTVlNDJjOTYwYmU5YTJlM2FlNGQ1MzUyNTQyOTg5NGMwMDllMGZhYTE2YTg0ZWFkYzdkNjVjYzZiYyIsImlhdCI6MTYzODQ4MjE1NSwibmJmIjoxNjM4NDgyMTU1LCJleHAiOjE2Mzg1Njg1NTUsInN1YiI6Ijc2OTEwODYiLCJhY2NvdW50X2lkIjoyOTg1NTAwMiwic2NvcGVzIjpbInB1c2hfbm90aWZpY2F0aW9ucyIsImNybSIsIm5vdGlmaWNhdGlvbnMiXX0.VsfizVK-_Y_qxnZE9GU5MHNjM9GCYdcAo483bt9bb1aEIsiJINHe7KB0XzKuryP7EHyXhUBDi_ebRTeOHsLltqQ3zcBPwJhVkkP4IwEJKAx7riYuG6etstKt_7ooHqRdtLkkeZwjevLb4y0KRRrS-d5NJPdxzgfHt6vcSYv7uimLr8822ci9YBMDPDkgkIlCsjbK-AdM0m5DJyN1Ig-Jdr2WcFU54qYft2kr4UHfyJGF7QWePJTfkVSWFblObZOGnXYCEsN6Fd2opKH2sZiq5VTMv9_nDIYhkZQPZu_77oL1pWlAYFXlXtMSeUqzVg4LoiAbB2Ifc_U6Oak31h1Adw';
+        $access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjJhY2JiM2YwYTVlZjE3YWQ3OTFjNTYwOTRjZWU1OTNmMDUxMWIzNDFiZTZjNzY4M2IxMjM1NmM1ZTJlZWU3OWJmZDZiOTdhM2IyNjgyNWFmIn0.eyJhdWQiOiJmZWE0M2RlNi1mYzIwLTQwZjUtOTg5MC1hMzI0NjM1ZWRlNDAiLCJqdGkiOiIyYWNiYjNmMGE1ZWYxN2FkNzkxYzU2MDk0Y2VlNTkzZjA1MTFiMzQxYmU2Yzc2ODNiMTIzNTZjNWUyZWVlNzliZmQ2Yjk3YTNiMjY4MjVhZiIsImlhdCI6MTYzODc5NzgwMywibmJmIjoxNjM4Nzk3ODAzLCJleHAiOjE2Mzg4ODQyMDMsInN1YiI6Ijc2OTEwODYiLCJhY2NvdW50X2lkIjoyOTg1NTAwMiwic2NvcGVzIjpbInB1c2hfbm90aWZpY2F0aW9ucyIsImNybSIsIm5vdGlmaWNhdGlvbnMiXX0.RM8z7J08HGlbH0Ft4sRzT9mEhwpS7Ria1fdQArV_bXYjxno2O_IoyHre8QHSsuyP5_FyEifiww71aPzO3mw38hi-hiyZ2-KTL0Ll9QCKXabPhttw5Y7nftqkWD3mcFGnN2DXeH09J3s6SSdyhP736g3n5EkhQ5oa01IHO8SEEknr95Dvo3p-R2VI_B0f60Hz1ocJjESKQtdgmU8agkYEk_r8wJxuT1uMvTgIK2QlU6a_1bas8XFld7v3H7vmAGOAGNKYaF-axR_V_5hW5nmf-2uTtycrTlmGg6V1kwmGIXKAKs9DOlKc1MBXH8b4POjmGHd7BgLZcO4uPHKqXICO2g';
         $headers = [
             'Content-Type:application/json',
             'Authorization: Bearer ' . $access_token
@@ -40,6 +40,9 @@ class HTTPRequester {
         //Сохраняем дескриптор сеанса cURL
         /** Устанавливаем необходимые опции для сеанса cURL  */
         curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $curl, CURLOPT_FOLLOWLOCATION, true );
+        curl_setopt( $curl, CURLOPT_CAINFO, 'cacert.pem' );
+
         curl_setopt( $curl, CURLOPT_USERAGENT, 'amoCRM-oAuth-client/1.0' );
         curl_setopt( $curl, CURLOPT_URL, $url );
         curl_setopt( $curl, CURLOPT_HTTPHEADER, $headers );
@@ -49,6 +52,11 @@ class HTTPRequester {
         curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, 1 );
         curl_setopt( $curl, CURLOPT_SSL_VERIFYHOST, 2 );
         $response = curl_exec( $curl );
+//                if ( curl_errno( $curl ) ) {
+//
+//           echo curl_error( $curl );
+//
+//        }
 
         curl_close( $curl );
         return $response;
